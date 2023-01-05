a04c017d520de9ec6c973f814b245681ad1ac4b6
Anurag Sharma <71688639+anuragsaitiya@users.noreply.github.com>
2023-01-05 14:01:36 +0530

    Add files via upload
    first commit

--- src/KK/Array.java
+++ src/KK/Array.java
@@ -1,2 +1,24 @@
-package KK;public class Array {
+package KK;
+import java.util.Scanner;
+
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int l = s.nextInt();
+        int[] arr = new int[l];
+        Input(arr);
+        print(arr);
+    }
+    public static void Input(int[] arr) {
+        Scanner s = new Scanner(System.in);
+            for(int z=0 ; z<arr.length ; z++){
+                arr[z] = s.nextInt();
+            }
+    }
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
 }
